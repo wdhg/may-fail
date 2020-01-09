b6de28653a83d455172a0ddc8e6204eda027a559
wdhg <wdhgrant@gmail.com>
2020-01-09 10:57:36 +0000

    implement Applicative typeclass for MayFail

--- Main.hs
+++ Main.hs
@@ -8,3 +8,15 @@ instance Functor MayFail where
     = Ok $ func result
   fmap _ (Fail message)
     = Fail message
+
+instance Applicative MayFail where
+  pure result
+    = Ok result
+  Fail message1 <*> Fail message2
+    = Fail $ message1 ++ " and " ++ message2
+  Fail message <*> _
+    = Fail message
+  _ <*> Fail message
+    = Fail message
+  Ok func <*> Ok value
+    = Ok $ func value
